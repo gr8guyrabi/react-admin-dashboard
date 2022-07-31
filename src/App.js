758a995316601c6294b01b9b5c9397f459d37777
Rabi Maharjan <rabi.maharjan015@gmail.com>
2022-07-31 15:22:49 +0545

    setup: tailwind, craco config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+import './App.css'
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return (
+        <h1 className="text-3xl underline">Hello World!</h1>
+    )
 }
 
 export default App
